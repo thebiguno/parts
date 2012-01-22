86b1d96cbb02abb0d58291353b30b21dfbaa11c8
Warren Janssens <warren.janssens@gmail.com>
2012-01-22 13:59:35 -0700

    adding a mybatis mapper

---
+++ src/ca/digitalcave/parts/data/PartsMapper.java
@@ -0,0 +1,36 @@
+package ca.digitalcave.parts.data;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import ca.digitalcave.parts.model.Attribute;
+import ca.digitalcave.parts.model.Category;
+import ca.digitalcave.parts.model.Part;
+
+public interface PartsMapper {
+	/**
+	 * Search for a tree of part categories and families and the number of parts that match the search terms.
+	 */
+	List<Category> search(@Param("terms") List<String> terms);
+	
+	/**
+	 * Get a list of attribute names by family.
+	 */
+	List<String> attributesByFamily(@Param("category") String category, @Param("family") String family);
+
+	/**
+	 * Get a list of parts by family.
+	 */
+	List<Part> partsByFamily(@Param("category") String category, @Param("family") String family);
+	
+	/**
+	 * Get a list of attributes for a part.
+	 */
+	List<Attribute> attributesByPart(@Param("partId") int partId);
+	
+	int newPartId();
+	void insert(@Param("attribute") Attribute attribute);
+	void update(@Param("oldAttribute") Attribute oldAttribute, @Param("newAttribute") Attribute newAttribute);
+	void remove(@Param("attribute") Attribute attribute);
+}
