af3308366467a6462d2a1cca789aeae5b8fedc94
Warren Janssens <warren.janssens@gmail.com>
2012-01-29 11:56:36 -0700

    implementing

--- src/ca/digitalcave/parts/security/PartsVerifier.java
+++ src/ca/digitalcave/parts/security/PartsVerifier.java
@@ -1,8 +1,12 @@
 package ca.digitalcave.parts.security;
 
+import java.util.Properties;
+
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Method;
+import org.restlet.data.Status;
 import org.restlet.security.Verifier;
 
 import ca.digitalcave.parts.PartsApplication;
@@ -16,21 +20,42 @@ public class PartsVerifier implements Verifier {
 	}
 	
 	public int verify(Request request, Response response) {
-		if (request.getChallengeResponse() == null) {
-			return RESULT_MISSING;
-		} else if (request.getChallengeResponse().getScheme().equals(ChallengeScheme.HTTP_BASIC)) {
-			if (check(request)) {
-				return RESULT_VALID;
-			} else {
-				return RESULT_INVALID;
-			}
+		final Request passwdRequest = new Request(Method.GET, "war://WEB-INF/passwd.properties");
+		final Response passwdResponse = application.getContext().getClientDispatcher().handle(passwdRequest);
+
+		if (passwdResponse.getStatus() == Status.CLIENT_ERROR_NOT_FOUND) {
+			// admin party!
+			return RESULT_VALID;
 		} else {
-			return RESULT_UNSUPPORTED;
+			if (request.getChallengeResponse() == null) {
+				return RESULT_MISSING;
+			} else if (request.getChallengeResponse().getScheme().equals(ChallengeScheme.HTTP_BASIC)) {
+				final Properties passwd = new Properties();
+				try {
+					passwd.load(passwdResponse.getEntity().getStream());
+				} catch (Exception e) {
+					;
+				}
+				passwd.getProperty(request.getClientInfo().getUser().getIdentifier());
+
+				final String identifier = request.getChallengeResponse().getIdentifier();
+				final String secret = new String(request.getChallengeResponse().getSecret());
+				final String storedSecret = passwd.getProperty(identifier);
+				
+				if (storedSecret.startsWith("OBF:")) {
+					return PasswordUtil.deobfuscate(storedSecret).equals(secret) ? RESULT_VALID : RESULT_INVALID;
+				} else if (storedSecret.startsWith("MD2:")
+						|| storedSecret.startsWith("MD5:")
+						|| storedSecret.startsWith("SHA-1:") 
+						|| storedSecret.startsWith("SHA-256:")
+						|| storedSecret.startsWith("SHA-512:")) {
+					return PasswordUtil.verify(storedSecret, secret.getBytes()) ? RESULT_VALID : RESULT_INVALID; 
+				} else {
+					return secret.equals(storedSecret) ? RESULT_VALID : RESULT_INVALID;
+				}
+			} else {
+				return RESULT_UNSUPPORTED;
+			}
 		}
-	}
-	
-	public boolean check(Request request) {
-		// TODO
-		return true;
 	}
 }
