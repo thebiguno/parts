7c244a0489bbb062c6dd44f2a0be0898bcc43f42
Warren Janssens <janssensw@richer.ca>
2013-10-13 10:22:02 -0600

    fixing compile errors

--- src/ca/digitalcave/parts/resource/mobile/FamilyResource.java
+++ src/ca/digitalcave/parts/resource/mobile/FamilyResource.java
@@ -1,14 +1,17 @@
 package ca.digitalcave.parts.resource.mobile;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.net.URLDecoder;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
-import org.json.JSONObject;
+import org.codehaus.jackson.JsonGenerator;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
 import org.restlet.representation.Variant;
+import org.restlet.representation.WriterRepresentation;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
@@ -37,27 +40,35 @@ public class FamilyResource extends ServerResource {
 			
 			final List<Part> parts = sqlSession.getMapper(PartsMapper.class).partsByFamily(category, family);
 			
-			final JSONObject result = new JSONObject();
-			for (Part part : parts) {
-				final Attribute partNumber = part.findAttribute("Manufacturer Part Number");
-				final Attribute description = part.findAttribute("Description");
-				final Attribute notes = part.findAttribute("Notes");
-				final Attribute quantity = part.findAttribute("Quantity In Stock");
-				JSONObject partObj = new JSONObject();
-				partObj.put("part", partNumber != null ? partNumber.getValue() : null);
-				partObj.put("description", description != null ? description.getValue() : null);
-				partObj.put("notes", notes != null ? notes.getValue() : null);
-				partObj.put("quantity", quantity != null ? Integer.parseInt(quantity.getValue()) : null);
-				final StringBuilder datasheets = new StringBuilder();
-				for (Attribute attribute : part.findAttributes("Datasheet")) {
-					datasheets.append("<a href='").append(attribute.getHref()).append("' target='_blank'>").append(attribute.getValue()).append("</a><br/>");
+			return new WriterRepresentation(MediaType.APPLICATION_JSON) {
+				@Override
+				public void write(Writer w) throws IOException {
+					final JsonGenerator g = application.getJsonFactory().createJsonGenerator(w);
+					g.writeStartObject();
+					g.writeBooleanField("success", true);
+					g.writeArrayFieldStart("data");
+					for (Part part : parts) {
+						g.writeStartObject();
+						final Attribute partNumber = part.findAttribute("Manufacturer Part Number");
+						final Attribute description = part.findAttribute("Description");
+						final Attribute notes = part.findAttribute("Notes");
+						final Attribute quantity = part.findAttribute("Quantity In Stock");
+						if (partNumber != null) g.writeStringField("part", partNumber.getValue());
+						if (description != null) g.writeStringField("description", description.getValue());
+						if (notes != null) g.writeStringField("notes", notes.getValue());
+						if (quantity != null) g.writeNumberField("quantity", Integer.parseInt(quantity.getValue()));
+						final StringBuilder datasheets = new StringBuilder();
+						for (Attribute attribute : part.findAttributes("Datasheet")) {
+							datasheets.append("<a href='").append(attribute.getHref()).append("' target='_blank'>").append(attribute.getValue()).append("</a><br/>");
+						}
+						g.writeStringField("datasheets", datasheets.toString());
+						g.writeEndObject();
+					}
+					g.writeEndArray();
+					g.writeEndObject();
+					g.close();
 				}
-				partObj.put("datasheets", datasheets.toString());
-				result.append("data", partObj);
-				result.put("success", true);
-			}
-
-			return new JsonRepresentation(result);
+			};
 		} catch (Exception e) {
 			throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e);
 		} finally {
